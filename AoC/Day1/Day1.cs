111dab9c9e68f81947bac448cb5688641b1ac658
Ben Edmondson <ben.edmondson99@gmail.com>
2022-12-07 16:22:06 +0000

    Day 1 input file parsed into a string

---
+++ AoC/Day1/Day1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Day 1");
+//magical energy
+//star fruit
+//50 needed minimum
+//calories
+//calculate total calories of each elf carried
+string file = File.ReadAllText("C:\\dev\\PayCalc\\AoC\\AoC\\Day1\\input.txt");
+Console.WriteLine(file);
+Console.ReadLine();
+
