7a32ba51bf4db583e0e276748f8de5725fb0150c
Ben Edmondson <ben.edmondson99@gmail.com>
2022-12-07 16:48:21 +0000

    Part 1 complete.

--- AoC/Day1/Day1.cs
+++ AoC/Day1/Day1.cs
@@ -4,7 +4,26 @@
 //50 needed minimum
 //calories
 //calculate total calories of each elf carried
-string file = File.ReadAllText("C:\\dev\\PayCalc\\AoC\\AoC\\Day1\\input.txt");
-Console.WriteLine(file);
-Console.ReadLine();
+//get file into array
+var file = File.ReadAllLines("C:\\dev\\PayCalc\\AoC\\AoC\\Day1\\input.txt");
+//swap to list, I like lists
+var strList = new List<string>(file);
+var bigCal = 0;
+var currentCal = 0;
+//iterate through list adding together calories once it hits a blank space.
+for (int i = 0; i < strList.Count; i++)
+{
+    try
+    {
+        currentCal = currentCal + Convert.ToInt32(strList[i]);
+    }
+    catch
+    {
+        if (currentCal > bigCal) { bigCal = currentCal; }
+        currentCal= 0;
+    }
+}
+Console.WriteLine(bigCal);
+Console.ReadKey();
+//Compare calories to the stored(current highest calories)
 
