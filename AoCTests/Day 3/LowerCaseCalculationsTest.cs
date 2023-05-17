462cca6b058776bd71fb86db9b9d8aa7892370eb
Ben Edmondson <ben.edmondson99@gmail.com>
2023-05-17 15:23:58 +0100

    Lowercase and Uppercase Calculations both pass

---
+++ AoCTests/Day 3/LowerCaseCalculationsTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using AoC;
+namespace AoCTests
+{
+    public class LowerCaseCalculationsTest
+    {
+        private LowercaseCalculation lowercaseCalculation = new LowercaseCalculation();
+
+        List<char> Inputs = new List<char>()
+        {
+            'p','L','P','v','t','s'
+        };
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void LowerCaseCalcGetsCorrectResult()
+        {
+            int result = lowercaseCalculation.GetLowercasePriorityTotal(Inputs);
+
+            Assert.That(result, Is.EqualTo(77));
+        }
+    }
+}
---
+++ AoCTests/Day 3/UpperCaseCalculationsTest.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using AoC;
+namespace AoCTests
+{
+    public class UppereCalculationsTest
+    {
+        private UppercaseCalculations uppercaseCalculation = new UppercaseCalculations();
+
+        List<char> Inputs = new List<char>()
+        {
+            'p','L','P','v','t','s'
+        };
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void UpperCaseCalcGetsCorrectResult()
+        {
+            int result = uppercaseCalculation.GetUppercasePriorityTotal(Inputs);
+
+            Assert.That(result, Is.EqualTo(80));
+        }
+    }
+}
