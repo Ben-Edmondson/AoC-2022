8dcfeecb911de6d4e5b3e4b4829d5739344fca76
Ben Edmondson <ben.edmondson99@gmail.com>
2023-05-31 10:13:55 +0100

    Day 4 prep

---
+++ AoC/Day4/Day4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC.Day4
+{
+    public class Day4
+    {
+        //ID number for each section
+        //some sections overlap
+        //some sections are completely contained by another
+        //how many pairs have this issue
+
+        //split the input into groups of 2
+        //compare the sections for overlap
+        //add to total pairs if there is complete overlap
+    }
+}
